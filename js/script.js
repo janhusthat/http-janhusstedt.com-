3ae9f727c0a16268aab514a1f8261d5fd5405b7c
janhusthat <janhusthat@gmail.com>
2017-03-30 11:59:32 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $( function() {
-    $( ".triangle,.circle,.square,.title,.phrase,.phrasealt,.phrasenew,.Rcap,.Ocap,.Wcap,.TYPOcap,.blackpaint2,.blackpaint3,.wilders,.img,.lawsof" ).draggable();
+    $( ".img" ).draggable();
     
     // $( "nav" ).draggable();  		// only blue nav circle draggable
     // $( ".arrow-up" ).draggable();  	// only yellow triangle draggable
@@ -8,52 +8,4 @@ $( function() {
     // $( "#my-image" ).draggable();	// only cogwheel draggable
 
   
-} );var canvas = document.getElementById('paint');
-var ctx = canvas.getContext('2d');
- 
-var sketch = document.getElementById('sketch');
-var sketch_style = getComputedStyle(sketch);
-canvas.width = 700;
-canvas.height = 900;
-
-var mouse = {x: 0, y: 0};
- 
-/* Mouse Capturing Work */
-canvas.addEventListener('mousemove', function(e) {
-  mouse.x = e.pageX - this.offsetLeft;
-  mouse.y = e.pageY - this.offsetTop;
-}, false);
-
-/* Drawing on Paint App */
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-
-ctx.strokeStyle = "black";
-function getColor(colour){ctx.strokeStyle =  colour;}
-
-function getSize(size){ctx.lineWidth = size;}
-
-
-//ctx.strokeStyle = 
-//ctx.strokeStyle = document.settings.colour[1].value;
- 
-canvas.addEventListener('mousedown', function(e) {
-    ctx.beginPath();
-    ctx.moveTo(mouse.x, mouse.y);
- 
-    canvas.addEventListener('mousemove', onPaint, false);
-}, false);
- 
-canvas.addEventListener('mouseup', function() {
-    canvas.removeEventListener('mousemove', onPaint, false);
-}, false);
- 
-var onPaint = function() {
-    ctx.lineTo(mouse.x, mouse.y);
-    ctx.stroke();
-};
-
-//img float around and shit
-$('.img').each(function(){
-    $(this).css({'marginTop' : getRandomInt(-100,600), 'marginTop' : getRandomInt(-100,600)});
-});
+}
